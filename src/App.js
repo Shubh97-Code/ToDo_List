7757453e2075a1f16f618c13a7fbc6f9b18e4dee
shubham kumar <56964959+Shubh97-Code@users.noreply.github.com>
2023-07-04 15:12:09 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,44 @@
-import React from "react";
-import "./App.css";
+import React,{useState} from "react";
+import ToDoList from "./ToDoList";
 
 function App(){
+  const [inputText,setinputText]=useState("");
+  const [items,setItems]=useState([]);
+
+  function handleChange(event){
+    const newValue = event.target.value;
+    setinputText(newValue);
+  }
+  function addItem(){
+    setItems(prevItems=>{
+      return[...prevItems,inputText];
+    });
+    setinputText("");
+  }
+  function deleteItem(id){
+    setItems(prevItems => {
+      return prevItems.filter((item, index) => {
+        return index !==   id;
+      });
+    });
+  }
   return(
-    <div className="App">      
-      Hello
+    <div className="container">      
+        <h1> To-Do List</h1>    
+      <div className="form">  
+        <input onChange={handleChange} type="text" value={inputText} />
+        <button onClick={addItem}> <span>Add</span>
+        </button>
+      </div>
+      <div>
+        <ul>
+       { items.map((todoItem,index)=>( <ToDoList key={index} id={index} text={todoItem}
+        onChecked={deleteItem}
+       />
+       )) }
+         
+        </ul>
+      </div>
     </div>
   );
 }
